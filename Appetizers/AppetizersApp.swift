cc096fbe96980f1ffc8cadbacf64aafa3c9121eb
Manish <manishagarwal428728@gmail.com>
2024-06-30 23:28:54 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Manish Agarwal on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
